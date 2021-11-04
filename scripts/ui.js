488d319ff4fa144cc022d362ed317936c4f770fb
keyur.m <keyur.m@productivet.com>
2021-11-04 21:17:39 +0530

    Moment Date

--- scripts/ui.js
+++ scripts/ui.js
@@ -6,10 +6,11 @@ class ChatUI {
         this.list.innerHTML = '';
     }
     render(data) {
-        const when = dateFns.distanceInWordsToNow(
-            data.created_at.toDate(),
-            { addSuffix:true }
-        );
+        // const when = dateFns.distanceInWordsToNow(
+        //     data.created_at.toDate(),
+        //     { addSuffix:true }
+        // );
+        const when = moment(data.created_at.toDate()).fromNow();
         const html = `
             <li class="list-group-item">
             <span class="username">${data.username}</span>
